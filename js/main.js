6fec87b67340f5e2d04a3d1fcbff55b4a6ae010f
Alicia Padrón <aliciaapadron@gmail.com>
2022-03-29 09:27:00 +0200

    Create a minimal layout + Generate a random number + paint the result

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 'use strict';
+const select = document.querySelector('.js_select');
+const button = document.querySelector('.js_button');
+const message = document.querySelector('.js_message');
+
+//selecciono el número que quiero apostar y le doy a jugar
+//click en jugar genera un número aleatorio
+//si el número aleatorio es igual al seleccionado mensaje en texto "Has ganado el doble de lo apostado . :)"
+//sino "Has perdido lo apostado . :("
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+function winnerOrNot() {
+  let giveMeNumber = getRandomNumber(6);
+  if (giveMeNumber == select.value) {
+    message.innerHTML = 'Has ganado el doble de lo apostado :).';
+  } else {
+    message.innerHTML = 'Has perdido lo apostado :(.';
+  }
+}
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  winnerOrNot();
+});
 
 // function addSmileTitle() {
 //   title.innerHTML = select.value;
