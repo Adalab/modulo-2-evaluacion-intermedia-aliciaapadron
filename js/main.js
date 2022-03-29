6185dfd01a3671dee1bde1ff3be0f452b8d54304
Alicia Padrón <aliciaapadron@gmail.com>
2022-03-29 21:33:55 +0200

    make all issues - css

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ const playButton = document.querySelector('.js_playButton');
 const howMuchMoney = document.querySelector('.js_howMoney');
 const message = document.querySelector('.js_message');
 const totalMoney = document.querySelector('.js_totalMoney');
+const whoWins = document.querySelector('.js_whoWins');
 
 //el saldo debe estar por defecto en 50. El valor del saldo debe ser 50 al iniciar el juego
 //se debe ir actualizando
-const initialNumber = 50;
+let initialNumber = 50;
 totalMoney.innerHTML = `Saldo: ${initialNumber}`;
 //generar un número aleatorio del 1 al 6
 //comparar el número con el seleccionado
@@ -19,44 +20,86 @@ function getRandomNumber(max) {
 //si pierdo restará lo apostado
 //si gano sumará al saldo total
 function resultPossitive() {
-  let totalNumber = parseInt(initialNumber) + parseInt(howMuchMoney.value) * 2;
+  initialNumber = parseInt(initialNumber) + parseInt(howMuchMoney.value) * 2;
 
-  totalMoney.innerHTML = `Saldo: ${totalNumber}`;
+  totalMoney.innerHTML = `Saldo: ${initialNumber}`;
 }
 function resultNegative() {
-  let totalNumber = parseInt(initialNumber) - parseInt(howMuchMoney.value);
+  initialNumber = parseInt(initialNumber) - parseInt(howMuchMoney.value);
 
-  totalMoney.innerHTML = `Saldo: ${totalNumber}`;
+  totalMoney.innerHTML = `Saldo: ${initialNumber}`;
 }
 function giveText(text) {
   message.innerHTML = text;
+}
+function giveSecondText(textSecond) {
+  whoWins.innerHTML = textSecond;
 }
 
 function randomNumber() {
   let giveMeNumber = getRandomNumber(6);
   if (giveMeNumber == selectNumber.value) {
     giveText('Has ganado el doble de lo apostado :D');
+    giveSecondText('Felicidades, ¡has haganado!');
     resultPossitive();
   } else {
     giveText('Has perdido lo apostado :(');
+    giveSecondText('Ups...te ha ganado una máquina');
     resultNegative();
+  }
+}
+
+//no se puede apostar + de lo que tengo
+//el juego finaliza cuando totalMoney es = 200 // no se puede apostar +de 100
+
+function canPlay(howMuchMoney, totalMoney) {
+  if (parseInt(howMuchMoney) <= parseInt(totalMoney)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// si totalMoney <= 200 se ejecutará la función randonNumber //sino
+function isMinus200(totalMoney) {
+  if (parseInt(totalMoney) <= 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
+//cuando click en botón reinicar vuelve a salir jugar
+//en el select la primera opción
+//howMuchMoney se vacíe
+//totalMoney `Saldo: ${initialNumber}` initalNumber=50
+
+function handleResertButton(event) {
+  event.preventDefault();
+  if (playButton.innerHTML == 'Reiniciar juego') {
+    playButton.innerHTML = 'Jugar';
+    selectNumber.selectedIndex = 0;
+    howMuchMoney.value = '';
+    initialNumber = 50;
+    totalMoney.innerHTML = `Saldo: ${initialNumber}`;
   }
 }
 
 function handleClickButton(event) {
   event.preventDefault();
-  randomNumber();
+  let coin = totalMoney.innerHTML.split(' ');
+
+  if (canPlay(howMuchMoney.value, coin[1])) {
+    if (isMinus200(coin[1])) {
+      randomNumber();
+    } else {
+      playButton.innerHTML = 'Reiniciar juego';
+      totalMoney.innerHTML = 'Has llegado al tope,¡nos vemos otro día!';
+    }
+  } else {
+    playButton.innerHTML = 'Reiniciar juego';
+    totalMoney.innerHTML = 'No tienes sufienciente dinero, inténtalo de nuevo';
+  }
 }
 
 playButton.addEventListener('click', handleClickButton);
-
-//el juego finaliza cuando totalMoney es = 200 // no se puede apostar +de 100
-
-// //solo se puede apostar si totalMoney <= howMuchMoney
-// //si totalMoney es <=200
-// function maxMoney() {
-//   if (howMuchMoney.value < totalMoney) {
-//     totalMoney.innerHTML = 'No tienes suficiente dinero, inténtalo de nuevo';
-//     playButton.innerHTML = 'Reiniciar Juego';
-//   }
-// }
+playButton.addEventListener('click', handleResertButton);
