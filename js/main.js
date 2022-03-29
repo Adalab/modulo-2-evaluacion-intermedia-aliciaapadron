f913997a93bcbe8c965aadcb66d8db89a3a1209f
Alicia Padrón <aliciaapadron@gmail.com>
2022-03-29 10:31:22 +0200

    add fuction editDom

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const button = document.querySelector('.js_button');
 const message = document.querySelector('.js_message');
 const amountOfMoney = document.querySelector('.js_input');
 const money = document.querySelector('.js_money');
+const balance = document.querySelector('.js_balance');
 
 //selecciono el número que quiero apostar y le doy a jugar
 //click en jugar genera un número aleatorio
@@ -14,12 +15,23 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+//lo que yo escriba en amountOfMoney cuando le de a jugar
+//si el número aleatorio es igual al que he elegido, gano y si gano muliplique *2 el amountOfMoney
+//si pierdo el money = a 0
+
+function editDom(text, num) {
+  message.innerHTML = text;
+  balance.value = num;
+}
+
 function winnerOrNot() {
   let giveMeNumber = getRandomNumber(6);
   if (giveMeNumber == select.value) {
-    message.innerHTML = 'Has ganado el doble de lo apostado :).';
+    let total = parseInt(balance.value) + parseInt(amountOfMoney.value * 2);
+    editDom('Has ganado el doble de lo apostado :).', total);
   } else {
-    message.innerHTML = 'Has perdido lo apostado :(.';
+    let total = 0;
+    editDom('Has perdido lo apostado :(.', total);
   }
 }
 
