2d14cae03bb53821d8c95791d007826094aa54df
Alicia Padrón <aliciaapadron@gmail.com>
2022-03-29 08:47:33 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// function addSmileTitle() {
+//   title.innerHTML = select.value;
+// }
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// function addOrange() {
+//   main.classList.remove('yellow');
+//   main.classList.add('orange');
+// }
+
+// function addYellow() {
+//   main.classList.add('yellow');
+//   main.classList.remove('orange');
+// }
+
+// function changeBackground() {
+//   let giveMeNumber = getRandomInt(100);
+//   if (giveMeNumber % 2 !== 0) {
+//     addOrange();
+//   } else {
+//     addYellow();
+//   }
+// }
+
+// function handleClickButton(event) {
+//   event.preventDefault();
+//   addSmileTitle();
+//   changeBackground();
+// }
+
+// button.addEventListener('click', handleClickButton);
